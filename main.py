052bc66a3c0e84fc9a74a40eba0e9de813cff650
Anna Fiscenkova <91870533+Blazer-mm@users.noreply.github.com>
2022-09-07 13:04:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#1.programma
+a = 100
+b = 50
+c = int(input("c: "))
+if a > b and a > c:
+  print("a is greater than b and c")
+else:
+  print("a is greater than b but not c")
+#2.programma
+x = 200
+y = int(input("y: "))
+if x > y:
+  print("x is greater than y")
+elif x == y:
+  print("x and y are equal")
+else:
+  print("y is greater than x")
+#3.programma
+m = 30
+n = 40
+if m < n: print("n is greater than m")
